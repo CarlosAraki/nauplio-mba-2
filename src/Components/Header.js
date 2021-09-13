575c1be9c580e768ab566689ef2be0dcbef89b0f
Carlos Araki <carlosarak@gmail.com>
2021-09-13 01:58:57 -0300

    retirada de botao

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -78,11 +78,11 @@ class Header extends Component {
           </div>
         </div>
 
-        <p className="scrolldown">
+        {/* <p className="scrolldown">
           <a className="smoothscroll" href="#about">
             <i className="icon-down-circle"></i>
           </a>
-        </p>
+        </p> */}
       </header>
     );
   }
