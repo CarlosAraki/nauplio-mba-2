362df919d9f6f34411861c06d3e9017fc210b116
Carlos Araki <carlosarak@gmail.com>
2021-09-12 22:15:08 -0300

    feat: barbara about

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,22 +1,28 @@
 import React, { Component } from "react";
 import Fade from "react-reveal";
+import Slide from "react-reveal";
 
 class About extends Component {
+
+  getRandomColor() {
+    let letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  } 
+
+
   render() {
     if (!this.props.data) return null;
 
-    const name = this.props.data.name;
-    const profilepic1 = "images/" + this.props.data.image;
-    const bio = this.props.data.bio;
-    const street = this.props.data.address.street;
-    const city = this.props.data.address.city;
-    const state = this.props.data.address.state;
-    const zip = this.props.data.address.zip;
-    const phone = this.props.data.phone;
-    const email = this.props.data.email;
-    const resumeDownload = this.props.data.resumedownload;
     const project = this.props.data.project;
     const github = this.props.data.github;
+    const barbara = this.props.data.team.barbara;
+
+  
+
 
     return (
       <section id="about">
@@ -25,50 +31,50 @@ class About extends Component {
             <div className="three columns">
               <img
                 className="profile-pic"
-                src={profilepic1}
-                alt="Nordic Giant Profile Pic"
+                src={"images/" + barbara.profile}
+                alt="Barbara Pic"
               />
             </div>
             <div className="nine columns main-col">
-              <h2>About Me</h2>
-
-              <p>{bio}</p>
+              <h1>Bárbara Pires</h1>
+              <hr/>
+              <p>{barbara.bio}</p>
+              <h2>Hard Skills</h2>
+              <p>{'\u2022'+" "+barbara.hardskills1}</p>
+              <p>{'\u2022'+" "+barbara.hardskills2}</p>
+              <p>{'\u2022'+" "+barbara.hardskills3}</p>
+              <h2>Soft Skills</h2>
+              <p>{'\u2022'+" "+barbara.softskills1}</p>
+              <h2>Projetos</h2>
+              <p>{'\u2022'+" "+barbara.projetos1}</p>
+              <p>{'\u2022'+" "+barbara.projetos2}</p>
+              <p>{'\u2022'+" "+barbara.projetos3}</p>
+              <h2>Trabalhos Voluntários</h2>
+              <p>{'\u2022'+" "+barbara.tv1}</p>
               <div className="row">
                 <div className="columns contact-details">
-                  <h2>Contact Details</h2>
-                  <p className="address">
-                    <span>{name}</span>
-                    <br />
-                    <span>
-                      {street}
-                      <br />
-                      {city} {state}, {zip}
-                    </span>
-                    <br />
-                    <span>{phone}</span>
-                    <br />
-                    <span>{email}</span>
-                  </p>
+                  
                 </div>
                 <div className="columns download">
-                  <p>
-                    <a href={resumeDownload} className="button">
-                      <i className="fa fa-linkedin"></i> LinkedIn
-                    </a>
-                    <hr />
-                  <Fade bottom duration={2000}>
-                    <ul className="social">
-                      <a href={project} className="button btn project-btn">
-                        <i className="fa fa-book"></i>Project
-                      </a>
-                      <a href={github} className="button btn github-btn">
-                        <i className="fa fa-github"></i>Github
-                      </a>
-                    </ul>
-                  </Fade>
-                  </p>
+                  
                 </div>
               </div>
+              <p>
+                <a href={barbara.linkedin} className="button">
+                  <i className="fa fa-linkedin"></i> LinkedIn
+                </a>
+                <hr />
+              <Fade bottom duration={2000}>
+                <ul className="social">
+                  <a href={project} className="button btn project-btn">
+                    <i className="fa fa-book"></i>Project
+                  </a>
+                  <a href={github} className="button btn github-btn">
+                    <i className="fa fa-github"></i>Github
+                  </a>
+                </ul>
+              </Fade>
+              </p>
             </div>
           </div>
         </Fade>
